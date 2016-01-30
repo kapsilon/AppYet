ac23aaf8c42bbb308a50c5b2496e4551a8daad2d
Sergey Kapustin <kapsilon47@gmail.com>
2016-01-30 03:35:54 +0300

    Moved static social page

---
+++ vk-mobile3.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Самое интересное сегодня в нашем ВК</title>
+</head>
+<body>
+<h3>#DarkKnightReturns #СуперКомандаСанджая #Проповедник #Флэш #ДокторКто</h3>
+
+<div id="vk_post_-74121510_7441"></div><script type="text/javascript">  (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//vk.com/js/api/openapi.js?117"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'vk_openapi_js'));  (function() {    if (!window.VK || !VK.Widgets || !VK.Widgets.Post || !VK.Widgets.Post("vk_post_-74121510_7441", -74121510, 7441, 'wJU7vfchqk-HBBUWRISqiPj85jXA', {width: 680})) setTimeout(arguments.callee, 50);  }());</script>
+
+<div id="vk_post_-74121510_7445"></div><script type="text/javascript">  (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//vk.com/js/api/openapi.js?117"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'vk_openapi_js'));  (function() {    if (!window.VK || !VK.Widgets || !VK.Widgets.Post || !VK.Widgets.Post("vk_post_-74121510_7445", -74121510, 7445, 'KUvcESj9ICLuJGt4UwG5zsZAd8yy', {width: 680})) setTimeout(arguments.callee, 50);  }());</script>
+
+<div id="vk_post_-74121510_7447"></div><script type="text/javascript">  (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//vk.com/js/api/openapi.js?117"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'vk_openapi_js'));  (function() {    if (!window.VK || !VK.Widgets || !VK.Widgets.Post || !VK.Widgets.Post("vk_post_-74121510_7447", -74121510, 7447, 'oz10zlU-K85o7GdWOmZk4Li9yXBo', {width: 680})) setTimeout(arguments.callee, 50);  }());</script>
+
+<div id="vk_post_-74121510_7457"></div><script type="text/javascript">  (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//vk.com/js/api/openapi.js?117"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'vk_openapi_js'));  (function() {    if (!window.VK || !VK.Widgets || !VK.Widgets.Post || !VK.Widgets.Post("vk_post_-74121510_7457", -74121510, 7457, 'GcFLye9vEwCsab3LzqsOaokiu0Z1', {width: 680})) setTimeout(arguments.callee, 50);  }());</script>
+
+<div id="vk_post_-74121510_7459"></div><script type="text/javascript">  (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//vk.com/js/api/openapi.js?117"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'vk_openapi_js'));  (function() {    if (!window.VK || !VK.Widgets || !VK.Widgets.Post || !VK.Widgets.Post("vk_post_-74121510_7459", -74121510, 7459, 'ooOpyCNRmZRY5oEgtYmYftZoNj6I', {width: 680})) setTimeout(arguments.callee, 50);  }());</script>
+
+
+
+<?php
+
+/*
+include '/wp-includes/post.php';
+include '/wp-includes/plugin.php';
+
+
+$mypostid = 7729;
+$my_post_obj = get_post( $my_post_id );
+$the_content_filtered = apply_filters( 'the_content', $my_post_obj->post_content );
+echo str_replace( ']]>', ']]&gt;', $the_content_filtered );
+*/
+
+?>
+
+</body>
+</html>
